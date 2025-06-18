35029538a8d179e70ed8978bb8ee3dd9bff9ce20
Daniel Lwetabe <dannylwe11@gmail.com>
2025-06-19 02:06:13 +0300

    simple response

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func message() string {
+	return "this is a test"
+}
+
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, message())
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", messageHandler)
+	fmt.Println("starting server on port 9002")
+	http.ListenAndServe(":9002", nil)
 }
